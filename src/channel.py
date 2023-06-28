ece0dd85157e2b3a41bd6f647395e4285e6baa34
Andrey Ryazanov <ark2635@gmail.com>
2023-06-28 13:37:44 +0300

    homework_3

--- src/channel.py
+++ src/channel.py
@@ -11,7 +11,17 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала.
-    Дальше все данные будут подтягиваться по API."""
+    Дальше все данные будут подтягиваться по API.
+    Атрибуты:
+        channel_id (str): Идентификатор канала.
+        channel_title (str): Название канала.
+        channel_description (str): Описание канала.
+        channel_url (str): Ссылка на канал.
+        channel_subs (int): Количество подписчиков канала.
+        video_count (int): Количество видео на канале.
+        chanel_views (int): Общее количество просмотров канала.
+        """
+
         self.youtube = self.get_service()
         self.channel_id = channel_id
         self.channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
@@ -19,9 +29,62 @@ class Channel:
         self.title = channel_info['snippet']['title']
         self.channel_description = channel_info['snippet']['description']
         self.url = f'https://www.youtube.com/channel/{self.channel_id}'
-        self.channel_subs = channel_info["statistics"]["subscriberCount"]
+        self.channel_subs = int(channel_info["statistics"]["subscriberCount"])
         self.video_count = channel_info["statistics"]["videoCount"]
         self.chanel_views = channel_info["statistics"]["viewCount"]
+
+    def __str__(self):
+        """
+        Возвращает строковое название канала и ссылку на него
+        """
+        return f'{self.title, self.url}'
+
+    def __add__(self, other):
+        """
+        Возвращает сумму подписчиков на 2х каналах
+        """
+        return self.channel_subs + other.channel_subs
+
+    def __sub__(self, other):
+        """
+        Возвращает разность подписчиков на 2х каналах
+        """
+        return self.channel_subs - other.channel_subs
+
+    def __gt__(self, other):
+        """
+        проверяет больше ли подписчиков на первом канале относительно второго
+        возвращает bool значение True если больше, False если меньше
+        """
+        return self.channel_subs > other.channel_subs
+
+    def __ge__(self, other):
+        """
+        проверяет больше или столько же подписчиков на первом канале относительно второго
+        возвращает bool значение True если больше или равно, False если меньше
+        """
+        return self.channel_subs >= other.channel_subs
+
+    def __lt__(self, other):
+        """
+        проверяет меньше ли подписчиков на первом канале относительно второго
+        возвращает bool значение True если меньше, False если больше
+        """
+        return self.channel_subs < other.channel_subs
+
+    def __le__(self, other):
+        """
+        проверяет меньше или равно подписчиков на первом канале относительно второго
+        возвращает bool значение True если меньше или равно, False если больше
+        """
+        return self.channel_subs <= other.channel_subs
+
+    def __eq__(self, other):
+        """
+        проверяет равенство подписчиков на 2х каналах
+        возвращает bool значение True если равно, False если не равно
+        """
+        return self.channel_subs == other.channel_subs
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
