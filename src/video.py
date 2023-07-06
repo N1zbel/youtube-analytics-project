9eb8f821dab3dd5e87f5f6d9c6126fee40a3cda2
Andrey Ryazanov <ark2635@gmail.com>
2023-07-06 21:12:40 +0300

    homework_4 update

--- src/video.py
+++ src/video.py
@@ -11,27 +11,19 @@ class Video:
         view_count (int): Количество просмотров видео.
         like_count (int): Количество лайков видео.
         """
-        self.video_id = video_id
-        self.title = None
-        self.url = None
-        self.view_count = None
-        self.like_count = None
-        self.video_data()
-
-    def __str__(self) -> str:
-        """Возврат строкового значения названия канала"""
-        return self.title
-
-    def video_data(self):
-        """Получает данные о видео"""
         youtube = Channel.get_service().videos().list(
-            part='snippet,statistics', id=self.video_id
+            part='snippet,statistics', id=video_id
         ).execute()
         video_data = youtube.get('items')[0]
+        self.video_id = video_id
         self.title = video_data.get('snippet').get('title')
         self.url = f'https://www.youtube.com/watch?v={self.video_id}'
         self.view_count = int(video_data.get('statistics').get('viewCount'))
         self.like_count = int(video_data.get('statistics').get('likeCount'))
+
+    def __str__(self) -> str:
+        """Возврат строкового значения названия канала"""
+        return self.title
 
 
 class PLVideo(Video):
@@ -41,4 +33,4 @@ class PLVideo(Video):
         video_id (str): Идентификатор видео.
         playlist_id (str): Идентификатор плейлиста."""
         super().__init__(video_id)
-        self.__playlist_id = playlist_id
+        self.playlist_id = playlist_id
